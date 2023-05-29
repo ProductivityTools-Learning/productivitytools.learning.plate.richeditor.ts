fb9e2d34104424ab88eccf354989e077332d068b
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-05-29 15:56:59 +0200

    simple editor

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
-import React from 'react';
-
+import React from "react";
+import { Plate, TEditableProps } from "@udecode/plate";
 
 function App() {
+  const editableProps: TEditableProps = {
+    placeholder: "Type...",
+  };
   return (
     <div className="App">
-    pawel
+      <Plate editableProps={editableProps} />
     </div>
   );
 }
