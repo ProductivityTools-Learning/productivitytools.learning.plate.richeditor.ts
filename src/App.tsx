a5351f099ef850389ff96f9a7119aecaa30e5842
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-05-30 10:53:54 +0200

    headings

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,24 @@
-import React from "react";
-import { Plate, PlateProvider, TEditableProps } from "@udecode/plate";
+import React, { useMemo } from "react";
+import { Plate, createParagraphPlugin, createHeadingPlugin, PlateProvider, TEditableProps ,createPlateUI} from "@udecode/plate";
 import { createMyPlugins, MyEditor, MyPlatePlugin, MyValue } from "./typescript/plateTypes";
-import { Toolbar } from './toolbar/Toolbar';
-import { ToolbarButtons } from './ToolbarButtons';
-
-
+import { Toolbar } from "./toolbar/Toolbar";
+import { ToolbarButtons } from "./ToolbarButtons";
 
 function App() {
+  const plugins = useMemo(
+    () =>
+      createMyPlugins([createParagraphPlugin(), createHeadingPlugin()], {
+        components: createPlateUI(),
+      }),
+    []
+  );
+
   const editableProps: TEditableProps = {
     placeholder: "Type...",
   };
   return (
     <div className="App">
-      <PlateProvider<MyValue>>
+      <PlateProvider<MyValue> plugins={plugins}>
         <Toolbar>
           <ToolbarButtons />
         </Toolbar>
