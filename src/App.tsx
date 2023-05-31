ecdb3fc7e9f52e4f638cebbd8f0f2b58f1f007fe
Pawel Wujczyk <pwujczyk@hotmail.com>
2023-05-31 05:49:45 +0200

    forced layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,25 @@
 import React, { useMemo } from "react";
 import {
-  createBasicElementsPlugin,
-  createExitBreakPlugin,
-  createResetNodePlugin,
-  createSoftBreakPlugin,
+  createBasicElementsPlugin,//h1, quote, code
+  createResetNodePlugin,//h1, quote, code
+  createSoftBreakPlugin,//h1, quote, code
   Plate,
   PlateProvider,
-  TEditableProps
+  TEditableProps,
+  createNormalizeTypesPlugin,//forced layout
+  createTrailingBlockPlugin,//forced layout
+  ELEMENT_H1,//forced layout
+  createPlateUI,
+  ELEMENT_CODE_BLOCK,
+  CodeBlockElement,
+  createExitBreakPlugin,
+  createHeadingPlugin
 } from '@udecode/plate';
+
+import { trailingBlockPlugin } from './trailing-block/trailingBlockPlugin';//forced layout
+import { forcedLayoutPlugin } from './forced-layout/forcedLayoutPlugin';//forced layout
+import { withStyledPlaceHolders } from './placeholder/withStyledPlaceHolders';
+
 
 
 import { plateUI } from './common/plateUI';
@@ -17,6 +29,14 @@ import { Toolbar } from "./toolbar/Toolbar";
 import { ToolbarButtons } from "./ToolbarButtons";
 import { resetBlockTypePlugin } from './reset-node/resetBlockTypePlugin';
 import { softBreakPlugin } from "./soft-break/softBreakPlugin";
+import { exitBreakPlugin } from './exit-break/exitBreakPlugin';
+
+
+let components = createPlateUI({
+  [ELEMENT_CODE_BLOCK]: CodeBlockElement,
+  // customize your components by plugin key
+});
+components = withStyledPlaceHolders(components);
 
 function App() {
   const plugins = useMemo(
@@ -27,9 +47,15 @@ function App() {
           createResetNodePlugin(resetBlockTypePlugin),
           createSoftBreakPlugin(softBreakPlugin),
           
+          createNormalizeTypesPlugin(forcedLayoutPlugin),//forced layout
+          createTrailingBlockPlugin(trailingBlockPlugin),//forced layout
+          createExitBreakPlugin(exitBreakPlugin),//forced layout
+          createHeadingPlugin(),//forced layout
+         
+
         ],
         {
-          components: plateUI,
+          components: components,
         }
       ),
     []
